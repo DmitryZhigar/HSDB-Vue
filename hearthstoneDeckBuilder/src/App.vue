9c75511fc565a7e73a16450c8aaa9c4899d7d49d
dmitryzhigar <dmitry.zhigar@gmail.com>
2019-01-22 22:23:10 +0300

    Added simple routing

--- hearthstoneDeckBuilder/src/App.vue
+++ hearthstoneDeckBuilder/src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div>
-    <Menu/>
+
   </div>
 </template>
 <script>
   import 'bootstrap/dist/css/bootstrap.css'
   import 'bootstrap-vue/dist/bootstrap-vue.css'
-  import BootstrapVue from 'bootstrap-vue'
-  import Menu from './Menu'
 
 
   export default {
     name: 'App',
     components: {
-      Menu
     }
   }
 </script>
--- hearthstoneDeckBuilder/src/main.js
+++ hearthstoneDeckBuilder/src/main.js
@@ -3,13 +3,39 @@
 import Vue from 'vue'
 import App from './App'
 import Menu from './Menu'
+import VueRouter from 'vue-router'
 
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 /* eslint-disable no-new */
+
+// компоненты, которые обрабатывают пути
+const NotFound = { template: '<h2>Page Not Found</h2>' }
+const Home = {
+// language=HTML
+  template: '<ul>\n' +
+  '    <li><router-link to="/" exact>Home</router-link></li>\n' +
+  '    <li><router-link to="/DeckBuilder">DeckBuilder</router-link></li>\n' +
+  '    <li><router-link to="/App">App</router-link></li>\n' +
+  '</ul>' }
+
+// маршруты, сопотавленные с компонентами
+const routes = [
+  { path: '/', component: Home },
+  { path: '/App', component: App },
+  { path: '/DeckBuilder', component: Menu },
+  { path: '*', component: NotFound }
+];
+// создаем объект маршрутизатора
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes
+});
+
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  router: router
 })
 
